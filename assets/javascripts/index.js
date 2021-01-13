473058a260879bb68c6f6188fd49593cc8c51f47
hachibu <raymondsohn@gmail.com>
2021-01-13 08:20:56 -0800

    Use context hook to simplify passing dimensions down

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,49 +1,49 @@
+const dimensions = {
+  height: 60,
+  width: 80
+};
+
+const DimensionsContext = React.createContext(dimensions);
+
 function App(props) {
   let css = {
     position: 'relative'
-  };
-  let dimensions = {
-    height: 60,
-    width: 80,
   };
   let canvasRef = React.useRef(null);
   let videoRef = React.useRef(null);
 
   return (
     <div style={css}>
-      <CanvasComponent ref={canvasRef} dimensions={dimensions} />
-      <VideoComponent ref={videoRef} dimensions={dimensions} />
-      <GridComponent canvasRef={canvasRef} videoRef={videoRef} dimensions={dimensions} />
+      <DimensionsContext.Provider value={dimensions}>
+        <CanvasComponent ref={canvasRef} />
+        <VideoComponent ref={videoRef} />
+        <GridComponent canvasRef={canvasRef} videoRef={videoRef} />
+      </DimensionsContext.Provider>
     </div>
   );
 }
 
 const CanvasComponent = React.forwardRef((props, ref) => {
+  let { height, width } = React.useContext(DimensionsContext);
   let css = {
     position: 'absolute',
     visibility: 'hidden',
-    height: `${props.dimensions.height * 2 - 1}px`,
-    width: `${props.dimensions.width * 2 - 1}px`,
+    height: `${height * 2 - 1}px`,
+    width: `${width * 2 - 1}px`,
   };
 
   return <canvas ref={ref} style={css}></canvas>;
 });
 
-CanvasComponent.propTypes = {
-  dimensions: PropTypes.exact({
-    height: PropTypes.number.isRequired,
-    width: PropTypes.number.isRequired,
-  }).isRequired
-};
-
 const VideoComponent = React.forwardRef((props, ref) => {
+  let { height, width } = React.useContext(DimensionsContext);
   let css = {
     position: 'absolute',
     top: '19px',
     left: '19px',
     zIndex: 10,
-    height: `${props.dimensions.height * 2 - 1}px`,
-    width: `${props.dimensions.width * 2 - 1}px`,
+    height: `${height * 2 - 1}px`,
+    width: `${width * 2 - 1}px`,
     border: '1px solid black',
   };
   let userMediaConfig = { audio: false, video: true };
@@ -57,19 +57,13 @@ const VideoComponent = React.forwardRef((props, ref) => {
   return <video ref={ref} autoPlay style={css}></video>;
 });
 
-VideoComponent.propTypes = {
-  dimensions: PropTypes.exact({
-    height: PropTypes.number.isRequired,
-    width: PropTypes.number.isRequired,
-  }).isRequired
-};
-
 function GridComponent(props) {
+  let { height, width } = React.useContext(DimensionsContext);
   let gridCss = {
     display: 'grid',
-    height: `${props.dimensions.height * 10}px`,
-    width: `${props.dimensions.width * 10}px`,
-    gridTemplateColumns: _.repeat('1fr ', props.dimensions.width),
+    height: `${height * 10}px`,
+    width: `${width * 10}px`,
+    gridTemplateColumns: _.repeat('1fr ', width),
     gridTemplateRows: 'auto',
     gridGap: '1px'
   };
@@ -78,7 +72,6 @@ function GridComponent(props) {
   React.useEffect(() => updateData());
 
   function updateData() {
-    let { height, width } = props.dimensions;
     let canvas = props.canvasRef.current.getContext('2d');
     let video = props.videoRef.current;
     canvas.drawImage(video, 0, 0, width, height);
@@ -96,13 +89,6 @@ function GridComponent(props) {
     </div>
   );
 }
-
-GridComponent.propTypes = {
-  dimensions: PropTypes.exact({
-    height: PropTypes.number.isRequired,
-    width: PropTypes.number.isRequired,
-  }).isRequired
-};
 
 if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
   alert('This browser does not support the Media Devices API.');
